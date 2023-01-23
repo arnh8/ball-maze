c5aa9ccfea4763458c574ff87f1d0ce8d8440b95
Arnold <arnold.hsieh8@gmail.com>
2023-01-23 17:26:47 -0500

    refactor, color adjustment

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import * as THREE from "three";
-import * as CANNON from "cannon-es";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { bodyToMesh } from "./three-conversion-utils";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+//import { RenderPixelatedPass } from "three/addons/postprocessing/RenderPixelatedPass.js";
+import { SMAAPass } from "three/addons/postprocessing/SMAAPass.js";
+
+import * as CANNON from "cannon-es";
 import { Quaternion } from "cannon-es";
-import CannonDebugger from "cannon-es-debugger"; //Debugger
-import { createMaze, hasConnection } from "./maze";
-import { bar } from "./bar";
+import CannonDebugger from "cannon-es-debugger";
+
 import tinycolor from "tinycolor2";
 
-//Dimensions
-//Maze parameters
+import { createMaze, hasConnection } from "./maze";
+import { bar } from "./bar";
+import { bodyToMesh } from "./three-conversion-utils";
+
+//Parameters
 const mazeLength = 15;
 const mazeFloorThickness = 0.25;
 const mazeWidth = 15;
@@ -17,32 +23,24 @@ const cells = 11; //cells x cells maze
 const oww = 0.5; //outer wall width
 const iww = oww / 2; //inner wall width
 const wh = 0.5; //wall height
-//Sphere paremeters
 const sphereRadius = 0.5;
 
-//0xa7fdff 0x164013 0x51814e
-
-//Materials
-const colorSeed = Math.floor(Math.random() * 0x444444 + 0x111111);
-const colors = tinycolor("#" + colorSeed.toString(16)).analogous();
-console.log(colors[0]);
-//const sphereMaterial = new THREE.MeshLambertMaterial({ color: 0xffffff });
+//Materials & colors
+const colors = getColors();
 const sphereMaterial = new THREE.MeshPhongMaterial({
     color: parseInt(colors[0].toHex(), 16),
     shininess: 150,
 });
 const mazeMaterial = new THREE.MeshLambertMaterial({
-    color: parseInt(colors[1].darken(15).toHex(), 16),
+    color: parseInt(colors[1].toHex(), 16),
 });
+
 const groundMaterial = new THREE.MeshLambertMaterial({
     color: parseInt(colors[2].toHex(), 16),
     side: THREE.DoubleSide,
 });
 
-//Scene setup
 const scene = new THREE.Scene();
-
-//Cannon setup
 const world = new CANNON.World({
     gravity: new CANNON.Vec3(0, -9.82, 0), // m/s²
 });
@@ -65,9 +63,7 @@ const camera = new THREE.OrthographicCamera(
     1,
     1000
 );
-
 camera.position.set(-26, 23, 26);
-//camera.lookAt(0, 110, 0);
 
 //SphereMesh
 const sphereWidthSegments = 21;
@@ -90,9 +86,9 @@ const sphereBody = new CANNON.Body({
 sphereBody.position.set(0, 20, 0);
 world.addBody(sphereBody);
 
+//Add Maze
 const matrix = createMaze(cells, cells);
 const cellwidth = (mazeLength - 2 * oww - (cells - 1) * iww) / cells;
-//Floor of maze (also the main body)
 const halfExtents = new CANNON.Vec3(
     mazeLength - 2 * oww,
     mazeFloorThickness,
@@ -116,7 +112,6 @@ mazeFloor.addShape(box1Shape, new CANNON.Vec3(mazeWidth - oww, zoffset, 0));
 mazeFloor.addShape(box1Shape, new CANNON.Vec3(-(mazeWidth - oww), zoffset, 0));
 
 //Inner vertical walls
-
 const vert = new CANNON.Box(new CANNON.Vec3(iww, wh, cellwidth));
 for (let cellNo = 0; cellNo < matrix[0].length * matrix[0].length; cellNo++) {
     //If cellNo is one of the cells on the far right skip it
@@ -156,10 +151,9 @@ for (let y = 0; y < matrix[0].length - 1; y++) {
 
     for (let x = 1; x < wallsBelow.length; x++) {
         if (wallsBelow[x] == 0) {
-            //0 detected, time to place a shape
-
+            //0 detected, place a shape
             if (wallsBelow[x - 1] == 0) {
-                //00, add cube
+                //00, add cube (single notch)
                 const horiz = new CANNON.Box(new CANNON.Vec3(iww, wh, iww));
                 const xoffset =
                     2 * x * (cellwidth + iww) - (mazeLength - 2 * oww + iww);
@@ -242,7 +236,6 @@ for (let y = 0; y < matrix[0].length - 1; y++) {
             }
         }
     }
-
     wallsBelow = [];
 }
 
@@ -294,37 +287,34 @@ for (let i = 0; i < mazeMesh.children.length; i++) {
 
 scene.add(mazeMesh);
 
-//Plane Setup
+//Ground
 const groundGeometry = new THREE.PlaneGeometry(1000, 1000);
 const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
 groundMesh.receiveShadow = true;
 groundMesh.rotation.x = Math.PI * -0.5;
 scene.add(groundMesh);
 
-//Ground
 const groundBody = new CANNON.Body({
     type: CANNON.Body.STATIC,
     shape: new CANNON.Plane(),
 });
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // make it face up
 world.addBody(groundBody);
+groundMesh.quaternion.copy(groundBody.quaternion);
 
 //Lighting
-/*
-const lighty = new THREE.SpotLight(0xffffff, 1, 0);
-lighty.position.set(2, 17, 2);
-lighty.angle = Math.PI / 2;
-lighty.castShadow = true;
-lighty.target = sphereMesh;
-scene.add(lighty)
-*/
+const spotLight = new THREE.SpotLight(0xffffff, 0.5, 0);
+spotLight.position.set(2, 17, 2);
+spotLight.angle = Math.PI / 2;
+spotLight.castShadow = true;
+scene.add(spotLight);
 
 const hemilight = new THREE.HemisphereLight(0xcccccc, 0x000000, 1);
 scene.add(hemilight);
 
 const sunLen = 15;
 const sunWid = 22;
-const sunLight = new THREE.DirectionalLight(0xffffff, 1);
+const sunLight = new THREE.DirectionalLight(0xffffff, 0.5);
 sunLight.castShadow = true;
 sunLight.position.set(10, 17, 9);
 sunLight.shadow.mapSize.width = 1024;
@@ -342,6 +332,21 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(bar());
 document.body.appendChild(renderer.domElement);
+
+//Postprocessing
+const composer = new EffectComposer(renderer);
+const renderPass = new RenderPass(scene, camera);
+composer.addPass(renderPass);
+
+const smaaPass = new SMAAPass(
+    window.innerWidth * renderer.getPixelRatio(),
+    window.innerHeight * renderer.getPixelRatio()
+);
+composer.addPass(smaaPass);
+
+//Playing with pixel postprocessing
+//const renderPixPass = new RenderPixelatedPass(2, scene, camera);
+//composer.addPass(renderPixPass);
 
 let wpress = false;
 let spress = false;
@@ -394,24 +399,24 @@ function onWindowResize() {
     camera.bottom = window.innerHeight / -50;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    composer.setSize(window.innerWidth, window.innerHeight);
 }
 
 //Debugging/Helpers
 //const axesHelper = new THREE.AxesHelper(15);
 //scene.add(axesHelper);
-const cannonDebugger = new CannonDebugger(scene, world); //Debugger
+//const cannonDebugger = new CannonDebugger(scene, world); //Debugger
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 5, 0);
 controls.update();
 //const sunHelper = new THREE.CameraHelper(sunLight.shadow.camera);
-//scene.add(sunhelper);
+//scene.add(sunHelper);
 
 function animate() {
     requestAnimationFrame(animate);
     sphereMesh.position.copy(sphereBody.position);
     sphereMesh.quaternion.copy(sphereBody.quaternion);
-    groundMesh.quaternion.copy(groundBody.quaternion);
 
     rotateFromInput();
 
@@ -420,7 +425,7 @@ function animate() {
 
     //cannonDebugger.update(); //Debugger
     world.fixedStep();
-    renderer.render(scene, camera);
+    composer.render();
 }
 
 function rotateFromInput() {
@@ -440,6 +445,32 @@ function rotateFromInput() {
     }
     const x = new Quaternion().setFromAxisAngle(vec, 0.2);
     mazeFloor.quaternion.slerp(x, 0.02, mazeFloor.quaternion);
+}
+
+function getColors() {
+    const colorSeed = tinycolor.random();
+    let colors;
+    if (Math.random() > 0.5) {
+        colors = tinycolor(colorSeed).monochromatic();
+    } else {
+        colors = tinycolor(colorSeed).analogous();
+    }
+
+    if (colors[1].getBrightness() > 120) {
+        const x = colors[1].getBrightness() - 50;
+        colors[1].darken(x * x * 0.0019);
+    } else if (colors[1].getBrightness() < 20) {
+        colors[1].brighten(10);
+    }
+
+    if (colors[2].getBrightness() > 120) {
+        const x = colors[2].getBrightness() - 50;
+        colors[2].darken(x * x * 0.0019 + 5);
+    } else if (colors[2].getBrightness() < 20) {
+        colors[2].brighten(10);
+    }
+
+    return colors;
 }
 
 animate();
