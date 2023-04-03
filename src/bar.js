663162fb9dbcd2b5694fe7ce054f16db899a1909
arnh8 <arnold.hsieh8@gmail.com>
2023-04-03 16:19:30 -0400

    refactor bar

--- src/bar.js
+++ src/bar.js
@@ -1,39 +1,36 @@
 export function bar() {
-    const top = document.createElement("div");
-    const button = document.createElement("button");
+  const top = document.createElement("div");
+  top.id = "bar";
 
-    button.innerHTML = "!";
+  const left = document.createElement("div");
+  left.innerText =
+    "Use WASD to tilt the maze! Refreshing generates a new maze with different colors.";
+  left.id = "left";
+  left.classList.add("shown", "info");
 
-    top.id = "bar";
+  const button = document.createElement("button");
+  button.innerHTML = "!";
+  button.addEventListener("click", () => {
+    button.classList.toggle("on");
+    right.classList.toggle("shown");
+    right.classList.toggle("hidden");
+    left.classList.toggle("shown");
+    left.classList.toggle("hidden");
+  });
 
-    const left = document.createElement("div");
-    left.innerText =
-        "Use WASD to tilt the maze! Refreshing generates a new maze with different colors.";
-    left.id = "left";
-    left.classList.add("hidden", "info");
+  const right = document.createElement("div");
+  right.id = "right";
+  right.innerText = "Click and drag to move the camera, scroll to zoom. ";
+  right.classList.add("shown", "info");
 
-    const right = document.createElement("div");
-    right.id = "right";
-    right.innerText = "Click and drag to move the camera, scroll to zoom. ";
-    right.classList.add("hidden", "info");
+  const githubLink = document.createElement("a");
+  githubLink.href = "https://github.com/arnh8/ball-maze";
+  githubLink.innerText = "github.com/arnh8/ball-maze";
+  right.appendChild(githubLink);
 
-    const githubLink = document.createElement("a");
-    githubLink.href = "https://github.com/arnh8/ball-maze";
-    githubLink.innerText = "github.com/arnh8/ball-maze";
+  top.appendChild(left);
+  top.appendChild(button);
+  top.appendChild(right);
 
-    right.appendChild(githubLink);
-    top.appendChild(left);
-    top.appendChild(button);
-    top.appendChild(right);
-
-    button.addEventListener("click", () => {
-        button.classList.toggle("on");
-
-        right.classList.toggle("shown");
-        right.classList.toggle("hidden");
-        left.classList.toggle("shown");
-        left.classList.toggle("hidden");
-    });
-
-    return top;
+  return top;
 }
